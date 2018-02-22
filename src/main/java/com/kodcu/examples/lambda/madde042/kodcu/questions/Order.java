57bffe36250e4ab8c9642fb7f7feb5614870ec3f
altuga@gmail.com <upux1234>
2018-02-22 09:39:12 +0300

    lambda eklendi

---
+++ src/main/java/com/kodcu/examples/lambda/madde042/kodcu/questions/Order.java
@@ -0,0 +1,14 @@
+package com.kodcu.examples.lambda.madde042.kodcu.questions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Order {
+
+    List<String> words = Arrays.asList("Merhaba", "Dunya");
+
+
+    //TODO 1 - words listesindekileri kelime uzunluklarina gore siralayiniz
+
+
+}
