9b3eade85063057708e11a4ab3776c82fd7cd4c5
altuga@gmail.com <upux1234>
2018-02-23 09:25:48 +0300

    lambda calismalari

---
+++ src/main/java/com/kodcu/examples/lambda/madde042/kodcu/questions/Main.java
@@ -0,0 +1,41 @@
+package com.kodcu.examples.lambda.madde042.kodcu.questions;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+
+       // TODO convert to List of Integer and print
+
+
+        List<Instant> listOfDates = new ArrayList<>();
+        listOfDates.add(Instant.parse("2017-10-23T10:12:35Z"));
+        listOfDates.add(Instant.parse("2017-11-23T10:12:35Z"));
+        listOfDates.add(Instant.parse("2018-12-23T10:12:35Z"));
+
+        // TODO print smaller then now
+
+
+        List<String> listOfNames = new ArrayList<>();
+        listOfNames.add("HELLO");
+        listOfNames.add("AFRICA");
+        listOfNames.add("hoW are you do'in");
+
+        // TODO convert to lowercase
+
+        // TODO use Streams in Java 8
+
+
+
+
+    }
+}
