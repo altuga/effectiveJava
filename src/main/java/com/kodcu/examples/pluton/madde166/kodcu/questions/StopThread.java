b125c9e5931f99449f1707b06c0f359dad592781
altuga@gmail.com <upux1234>
2018-02-28 00:41:53 +0300

    refactor edildi

--- src/main/java/com/kodcu/examples/pluton/madde166/kodcu/questions/StopThread.java
+++ src/main/java/com/kodcu/examples/pluton/madde166/kodcu/questions/StopThread.java
@@ -1,17 +1,22 @@
-// Broken! - How long would you expect this program to run? - Page 259
+
 package com.kodcu.examples.pluton.madde166.kodcu.questions;
 
 import java.util.concurrent.TimeUnit;
 
 public class StopThread {
-	private static boolean stopRequested;
+	private static volatile boolean stopRequested;
 
+
+	// Bu uygulama nasıl calisir ?
 	public static void main(String[] args) throws InterruptedException {
 		Thread backgroundThread = new Thread(new Runnable() {
 			public void run() {
 				int i = 0;
-				while (!stopRequested)
+				while (!stopRequested) {
 					i++;
+				}
+
+				System.out.println(i);
 			}
 		});
 		backgroundThread.start();
